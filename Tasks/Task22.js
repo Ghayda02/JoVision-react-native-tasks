c23399752cdccf9349cdb57b919353bfe0cb59d1
ghayda <you@example.com>
2024-05-19 19:46:32 +0300

    task22

---
+++ Tasks/Task22.js
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import { Text, View, TextInput } from 'react-native';
+
+
+const Task22=() => {
+  const [content, setContent] = useState('');
+  
+  return(
+    <View >
+    <Text style={{fontSize:30}}>RN Task22</Text>
+    <Text style={{fontSize:20}}>{content}</Text>
+    <MyFunctionPage changeTxt={setContent} />
+  </View> 
+  )
+}
+
+const MyFunctionPage=(props) => {
+
+    return(
+        <View>
+            <TextInput placeholder='Please Enter Text here'
+            onChangeText={(text) => props.changeTxt(text)}>
+            </TextInput>
+        </View>
+    )
+}
+
+export default Task22;
