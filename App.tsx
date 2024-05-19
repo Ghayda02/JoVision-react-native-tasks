5583720221034e1cd359e172caca95cfafb2d106
ghayda <you@example.com>
2024-05-19 04:34:23 +0300

    task18

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import react, { useState } from 'react';
 import { Text, View, Button, SafeAreaView } from 'react-native';
 import Task16 from './Tasks/Task16';
 import Task17 from './Tasks/Task17';
+import Task18 from './Tasks/Task18';
 
 
 const App=() => {
@@ -9,7 +10,8 @@ const App=() => {
   return(
     <SafeAreaView>
       {/* <Task16></Task16> */}
-      <Task17></Task17>
+      {/* <Task17></Task17> */}
+      <Task18></Task18>
     </SafeAreaView>
     
   )
---
+++ Tasks/Task18.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react';
+import {
+  SafeAreaView,
+  FlatList,
+  ActivityIndicator,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+class Task18 extends React.Component{
+  
+    constructor(){
+      super();
+      this.state = {
+        isloading: true
+      }
+    }
+
+    componentDidMount(){
+        setTimeout(()=>{
+            this.setState({isloading: false})}
+            ,5000)
+    }
+  
+    render(){
+      
+      return(
+        <View >
+          <Text style={{fontSize:30}}>RN Task18</Text>
+          {
+            this.state.isloading? 
+            <View>
+                <ActivityIndicator size={'large'}/>
+                <Text style={{fontSize:20}}>Loading...</Text>
+            </View>
+            :
+            <Text style={{fontSize:20}}>Ghayda</Text>
+          }
+          
+        </View>
+      )
+    }
+  }
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    }})
+  
+    export default Task18;
